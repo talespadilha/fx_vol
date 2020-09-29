959ed2dcec37374a3dee95d28d6e9ca47f2f20da
Tales Padilha <talespadilha@yahoo.com>
2020-09-29 10:55:04 +0100

    presentation 3

--- nominal_fx_vol.py
+++ nominal_fx_vol.py
@@ -10,6 +10,7 @@ os.chdir('/Users/talespadilha/Documents/Projects/fx_vol')
 
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 import general_functions as gf
 from arch import arch_model
 from nominal_fx_data import nominal_import
@@ -86,20 +87,31 @@ EMS, DMS = gf.markets_set(r_fx, path)
 
 #%% Taking a look jointly
 # Selecting the set of countries
-countries = EMS
+countries = EMS + DMS
 
 f = r_fx[countries].mean(axis=1)
 
 # Estimating the mean factor model
+    # First checking the AR behaviour:
+t = {}
+for country in countries:
+    res = est_ols_ar(r_fx[country], f)
+    t[country] = res.tvalues['lag']
+tstats = pd.Series(t)
+
 e={}
 b={}
 for country in countries:
-    #res = est_ols_ar(r_fx[country], f)
-    res = est_ols(r_fx[country], f)
+    if abs(tstats[country])>1.96:       
+        res = est_ols_ar(r_fx[country], f)
+    else:
+        res = est_ols(r_fx[country], f)
     e[country] = res.resid
     b[country] = res.params
 e_hat = pd.concat(e, axis=1)
 betas = pd.concat(b, axis=1)
+
+
 print(f"Median factor effect {betas.loc['f'].median()}.")
 betas.loc['f'].hist()
 e_corr = e_hat.corr()
@@ -162,4 +174,71 @@ b_series = pd.Series(b)
 
 xi = np.sqrt((N*T)/((N-1)/2)) * (a_series.sum()/b_series.sum())
 xi
+
+#%% Importing GEOVOL data
+x_daily = pd.read_csv(path+'geovols/daily_dm.csv', index_col=0)
+x_daily.index = pd.to_datetime(x_daily.index, format='%d/%m/%Y')
+s = pd.read_csv(path+'geovols/s_dm.csv', index_col=0)
+x_monthly = x_daily.resample('MS').mean()
+
+g = {}
+ccs = list(std_e.columns)
+#ccs.remove('ILS')
+for country in ccs:
+    g[country] = (s.loc[country].values * x_daily) + 1 - s.loc[country].values
+g = pd.concat(g, axis=1)
+g.columns = g.columns.droplevel(1)
+
+
+corr_list(se2.corr()).hist()
+print(f"Mean e2 Corr: {corr_list(se2.corr()).mean()}")
+
+epsilon = se2 / g
+corr_list(epsilon.corr()).hist()
+print(f"Mean epsilon Corr: {corr_list(epsilon.corr()).mean()}")
+
+
+# Checking monthly
+g_monthly = {}
+for country in ccs:
+    g_monthly[country] = (s.loc[country].values * x_monthly) + 1 - s.loc[country].values
+g_monthly = pd.concat(g_monthly, axis=1)
+g_monthly.columns = g_monthly.columns.droplevel(1)
+
+
+se2_monthly = (std_e**2).resample('MS').mean()
+corr_list(se2_monthly.corr()).hist()
+print(f"Mean e2 Corr: {corr_list(se2_monthly.corr()).mean()}")
+
+epsilon_m = se2_monthly/g_monthly
+corr_list(epsilon_m.corr()).hist()
+print(f"Mean epsilon Corr: {corr_list(epsilon_m.corr()).mean()}")
+
+
+#%% Testing for geovol
+psi = epsilon - 1
+
+
+# Testing for GEOVOL
+N = psi.shape[1]
+T = psi.shape[0]
+
+a=[]
+for i in range(N):
+    for j in range(i):
+        if i>j:
+            for t in range(T):
+                a.append(psi.iloc[t,i]*psi.iloc[t,j])
+a_series = pd.Series(a)
+
+b=[]
+for i in range(N):
+    for t in range(T):
+        b.append(psi.iloc[t,i]**2)
+b_series = pd.Series(b)
+
+xi = np.sqrt((N*T)/((N-1)/2)) * (a_series.sum()/b_series.sum())
+xi
+
+
 
